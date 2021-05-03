06ae4b2202180ba7b3203797b9765bb1218db1d9
Szymon <sszulinski@gmail.com>
2021-05-03 15:28:31 +0200

    add some new styles to subpage login

--- src/components/views/Login/Login.js
+++ src/components/views/Login/Login.js
@@ -6,23 +6,27 @@ const Login = () => (
   <div className={styles.root}>
     <div className='container'>
       <h2 className={styles.title}> autoryzacja i uprawnienia NOWOŚĆ </h2>
-      <h3 className={styles.loginTitle}> Logowanie dla pracowników </h3>
-      <form className={styles.formWrapper}>
-        <label htmlFor='email'> Email </label>
-        <input type='email' id='email' minLength='6' required />
-        <label htmlFor='password'> Hasło </label>
-        <input type='password' id='password' minLength='8' required />
-        <p>
-          {' '}
-          Nie pamiętasz hasła?{' '}
-          <a href='#' onClick={event => event.preventDefault()}>
-            {' Przypomnij hasło. '}{' '}
-          </a>
-        </p>
-        <Button className={styles.button} onClick={event => event.preventDefault()}>
-          Zaloguj się
-        </Button>
-      </form>
+      <div className={styles.window}>
+        <h1 className={styles.header}> nakiedy </h1>
+        <p> .com </p>
+        <h3 className={styles.loginTitle}> Logowanie dla pracowników </h3>
+        <form className={styles.formWrapper}>
+          <label htmlFor='email'> Email </label>
+          <input type='email' id='email' minLength='6' required />
+          <label htmlFor='password'> Hasło </label>
+          <input type='password' id='password' minLength='8' required />
+          <p>
+            {' '}
+            Nie pamiętasz hasła?
+            <a href='#' onClick={event => event.preventDefault()}>
+              {' Przypomnij hasło. '}{' '}
+            </a>
+          </p>
+          <Button className={styles.button} onClick={event => event.preventDefault()}>
+            Zaloguj się
+          </Button>
+        </form>
+      </div>
     </div>
   </div>
 );

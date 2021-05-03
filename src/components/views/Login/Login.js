2ee6b8dd570f0f794062bd1709f5a96a3a3075f9
Szymon <sszulinski@gmail.com>
2021-05-03 16:16:28 +0200

    finish styles for login subpage

--- src/components/views/Login/Login.js
+++ src/components/views/Login/Login.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import styles from './Login.module.scss';
 import Button from '../../common/Button/Button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClock } from '@fortawesome/free-solid-svg-icons';
 
 const Login = () => (
   <div className={styles.root}>
     <div className='container'>
       <h2 className={styles.title}> autoryzacja i uprawnienia NOWOŚĆ </h2>
       <div className={styles.window}>
-        <h1 className={styles.header}> nakiedy </h1>
+        <h1 className={styles.header}>
+          {' '}
+          <FontAwesomeIcon className={styles.icon} icon={faClock} /> nakiedy{' '}
+        </h1>
         <p> .com </p>
         <h3 className={styles.loginTitle}> Logowanie dla pracowników </h3>
         <form className={styles.formWrapper}>
